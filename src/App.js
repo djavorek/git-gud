85d9cf172e1d6fff31a49b621556315d78a08f1a
Javorek Dénes <javorek.denes@gmail.com>
2021-03-04 20:29:52 +0100

    Added react app, base structure

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+import Graph from './Graph/Graph.js';
+
+
+function App() {
+  return (
+    <div className="App">
+      <div className="Start">
+        <span>git-gud</span>
+      </div>
+      <Graph />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Question/Graph/Question.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './Question.css';
+
+class Question extends React.Component {
+
+	render() {
+		return (
+			<div>
+	
+			</div>
+		);
+	}
+}
+
+
+export default Question;
